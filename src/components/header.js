62a18c7c1aee575b85e1dfabf29d979d045a266c
Felix Peters <felix@felixpeters.me>
2021-04-23 11:12:04 +0200

    fix user menu popup

--- src/components/header.js
+++ src/components/header.js
@@ -110,7 +110,7 @@ export default function Header() {
                       >
                         <Menu.Items
                           static
-                          className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                          className="z-10 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                         >
                           <Menu.Item>
                             {({ active }) => (
