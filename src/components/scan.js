8e891d59089d71366e6818fc3d97d4544fed1309
Felix Peters <felix@felixpeters.me>
2021-04-30 11:15:25 +0200

    add static slider for 3D image viewing

--- src/components/scan.js
+++ src/components/scan.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useState } from "react"
 import classnames from "classnames"
 import PropTypes from "prop-types"
+import Slider from "rc-slider"
 
 function ScanImage({ images }) {
   const [showOverlay, setShowOverlay] = useState(true)
@@ -24,6 +25,23 @@ function ScanImage({ images }) {
         })}
         src={"/" + images.overlay}
       />
+      <div className="flex flex-row">
+        <Slider
+          min={1}
+          max={137}
+          defaultValue={119}
+          marks={{ 1: 1, 137: 137 }}
+          step={1}
+          railStyle={{ backgroundColor: "#5a67d8" }}
+          trackStyle={{ backgroundColor: "#5a67d8" }}
+          handleStyle={{
+            borderColor: "#5a67d8",
+          }}
+          dotStyle={{
+            borderColor: "#5a67d8",
+          }}
+        />
+      </div>
     </>
   )
 }
