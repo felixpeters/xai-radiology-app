e6b0d10bf863d6d466f223ef1b4abbb73c63e786
Felix Peters <felix@felixpeters.me>
2021-05-06 11:33:38 +0200

    add scan details and active AI badge

--- src/templates/scan.js
+++ src/templates/scan.js
@@ -1,7 +1,8 @@
 import React from "react"
+import classnames from "classnames"
 import { Fragment, useState } from "react"
 import { Dialog, Transition } from "@headlessui/react"
-import { XIcon } from "@heroicons/react/outline"
+import { XIcon, ArrowCircleRightIcon } from "@heroicons/react/outline"
 import { Link, graphql } from "gatsby"
 import {
   ClipboardListIcon,
@@ -71,6 +72,16 @@ function Scan({ data }) {
               </h1>
             </div>
             <div className="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
+              <span className="mr-4 inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-indigo-100 text-indigo-800 hover:bg-indigo-200 cursor-pointer">
+                <svg
+                  className="-ml-0.5 mr-1.5 h-2 w-2 text-indigo-400"
+                  fill="currentColor"
+                  viewBox="0 0 8 8"
+                >
+                  <circle cx={4} cy={4} r={3} />
+                </svg>
+                Active AI: Nodule detection & classification
+              </span>
               <button
                 onClick={() => setScanInfoOpen(true)}
                 type="button"
@@ -129,13 +140,79 @@ function Scan({ data }) {
                           </div>
                         </div>
                       </div>
-                      <div className="mt-6 relative flex-1 px-4 sm:px-6">
+                      <div className="mt-4 relative flex-1 px-4 sm:px-6">
                         {/* Replace with your content */}
                         <div className="absolute inset-0 px-4 sm:px-6">
-                          <div
-                            className="h-full border-2 border-dashed border-gray-200"
-                            aria-hidden="true"
-                          />
+                          <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+                            <dl className="sm:divide-y sm:divide-gray-200">
+                              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 ">
+                                <dt className="text-sm font-medium text-gray-500">
+                                  Priority
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
+                                  <span
+                                    className={classnames(
+                                      "px-2 inline-flex text-xs leading-5 font-semibold rounded-full",
+                                      {
+                                        "bg-red-100 text-red-800":
+                                          scan.priority.code == "High",
+                                        "bg-yellow-100 text-yellow-800":
+                                          scan.priority.code == "Medium",
+                                        "bg-green-100 text-green-800":
+                                          scan.priority.code == "Low",
+                                      }
+                                    )}
+                                  >
+                                    {scan.priority.code}:{" "}
+                                    {scan.priority.explanation}
+                                  </span>
+                                </dd>
+                              </div>
+                              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 ">
+                                <dt className="text-sm font-medium text-gray-500">
+                                  Patient
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
+                                  <div className="flex flex-col">
+                                    <div className="text-sm text-gray-900">
+                                      {scan.patient.name}
+                                    </div>
+                                    <div className="text-sm text-gray-900">
+                                      {scan.patient.sex}
+                                    </div>
+                                    <div className="text-sm text-gray-900">
+                                      {scan.patient.age} years
+                                    </div>
+                                    <button
+                                      type="button"
+                                      className="inline-flex w-40 items-center mt-2 px-4 py-2 border border-transparent rounded-full shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    >
+                                      <ArrowCircleRightIcon className="h-4 w-4 text-white mr-2" />
+                                      Open full EHR
+                                    </button>
+                                  </div>
+                                </dd>
+                              </div>
+                              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 ">
+                                <dt className="text-sm font-medium text-gray-500">
+                                  Procedure
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
+                                  <div className="flex flex-col">
+                                    <div className="text-sm text-gray-900">
+                                      {scan.procedure.name}
+                                    </div>
+                                    <div className="text-sm text-gray-900">
+                                      {scan.procedure.datetime}
+                                    </div>
+                                    <div className="text-sm text-gray-900">
+                                      {scan.procedure.reason}
+                                    </div>
+                                  </div>
+                                </dd>
+                              </div>
+                            </dl>
+                          </div>
                         </div>
                         {/* /End replace */}
                       </div>
@@ -154,11 +231,19 @@ export const query = graphql`
   query ScanbyId($scanId: String!) {
     scansJson(id: { eq: $scanId }) {
       id
+      priority {
+        code
+        explanation
+      }
       patient {
         name
+        sex
+        age
       }
       procedure {
         datetime
+        name
+        reason
       }
       images {
         raw
@@ -184,11 +269,19 @@ Scan.propTypes = {
   data: PropTypes.shape({
     scansJson: PropTypes.shape({
       id: PropTypes.string.isRequired,
+      priority: PropTypes.shape({
+        code: PropTypes.string,
+        explanation: PropTypes.string,
+      }),
       patient: PropTypes.shape({
         name: PropTypes.string,
+        sex: PropTypes.string,
+        age: PropTypes.number,
       }),
-      procecure: PropTypes.shape({
+      procedure: PropTypes.shape({
+        name: PropTypes.string,
         datetime: PropTypes.string,
+        reason: PropTypes.string,
       }),
       images: PropTypes.shape({
         raw: PropTypes.string,
