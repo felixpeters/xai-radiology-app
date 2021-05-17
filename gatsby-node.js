fdf5781b4a1670b1618b908cd212bd4a448abcd9
Felix Peters <felix@felixpeters.me>
2021-05-17 14:55:53 +0200

    clean up data model

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,20 @@
 const path = require("path")
 
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+    type ScansJson implements Node {
+      id: ID!
+    }
+
+    type NodulesJson implements Node {
+      id: ID!
+    }
+  `
+
+  createTypes(typeDefs)
+}
+
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
   const scanTemplate = path.resolve("src/templates/scan.js")
