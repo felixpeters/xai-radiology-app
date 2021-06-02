129ebb3444b50db6b65fc3f04e60f2f89e4eb352
Felix Peters <felix@felixpeters.me>
2021-06-02 13:13:26 +0200

    style predicted characteristics

--- src/components/noduleCharacteristics.js
+++ src/components/noduleCharacteristics.js
@@ -7,57 +7,36 @@ export default function NoduleCharacteristics({ data }) {
       <h3 className="text-lg leading-6 font-medium text-gray-900 mt-4">
         Predicted characteristics
       </h3>
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-6 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="table-auto min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Characteristic
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Prediction
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {data.map(classification => (
-                  <tr key={classification.classification.name}>
-                    <td className="w-1/4 px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm font-medium text-gray-900">
-                        {classification.classification.name}
-                      </div>
-                    </td>
-                    <td className="w-3/4 px-6 py-4 whitespace-nowrap">
-                      <Slider
-                        min={0}
-                        max={classification.classification.marks.length - 1}
-                        value={classification.value - 1}
-                        step={null}
-                        marks={{ ...classification.classification.marks }}
-                        trackStyle={{ backgroundColor: "#5a67d8" }}
-                        handleStyle={{
-                          borderColor: "#5a67d8",
-                        }}
-                        activeDotStyle={{
-                          borderColor: "#5a67d8",
-                        }}
-                        className="mb-4"
-                      />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+      <div className="mt-4 bg-white shadow overflow-hidden sm:rounded-md">
+        <ul className="divide-y divide-gray-200">
+          {data.map(classification => (
+            <li
+              key={classification.classification.name}
+              className="flex flex-row md:flex-col px-4 py-4 sm:px-6"
+            >
+              <div className="-mx-2 text-sm font-medium text-gray-900">
+                {classification.classification.name}
+              </div>
+              <div className="w-5/6 mx-auto">
+                <Slider
+                  min={0}
+                  max={classification.classification.marks.length - 1}
+                  value={classification.value - 1}
+                  step={null}
+                  marks={{ ...classification.classification.marks }}
+                  trackStyle={{ backgroundColor: "#5a67d8" }}
+                  handleStyle={{
+                    borderColor: "#5a67d8",
+                  }}
+                  activeDotStyle={{
+                    borderColor: "#5a67d8",
+                  }}
+                  className="my-4"
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   )
