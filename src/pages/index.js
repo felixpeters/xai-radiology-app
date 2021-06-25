ba19bd3c120f9fb52b8a0b0d59a422b4c84e1947
Felix Peters <felix@felixpeters.me>
2021-06-25 10:53:25 +0200

    add user ID to worklist page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { useState, createContext } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import WorkList from "../components/worklist"
+import UserStateContext from "../components/userContext"
 
-export default function Home() {
+export default function Home({ location }) {
+  const params = new URLSearchParams(location.search)
+  const pid = params.get("pid")
+  const [user, setUser] = useState(pid)
+
   return (
-    <Layout>
-      <SEO title="Worklist" />
-      <WorkList />
-    </Layout>
+    <UserStateContext.Provider value={user}>
+      <Layout>
+        <SEO title="Worklist" />
+        <WorkList />
+      </Layout>
+    </UserStateContext.Provider>
   )
 }
