f28bd5d410de69c709bbe2cf09242a56140f4f53
Felix Peters <felix@felixpeters.me>
2021-07-20 09:59:28 +0200

    Adjust instructions to match survey

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -45,6 +45,7 @@ export default function Instructions() {
                   </p>
                   <ul className="list-decimal list-inside mt-2">
                     <li>Open the first item on your worklist.</li>
+                    <li>Familiarize yourself with the application.</li>
                     <li>Classify all nodules.</li>
                     <li>Go back to the survey.</li>
                   </ul>
